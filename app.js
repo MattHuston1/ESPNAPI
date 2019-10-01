c395015ba9c9f36da4b3c836363b1e1171179e9f
Matt Huston <matthuston1@aol.com>
2019-10-01 12:17:16 -0600

    getting info and adding member info to page

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log('HEllO')
+let draftEndpoint = `http://fantasy.espn.com/apis/v3/games/ffl/seasons/2019/segments/0/leagues/1241838?view=mDraftDetail&view=mLiveScoring&view=mMatchupScore&view=mPendingTransactions&view=mPositionalRatings&view=mSettings&view=mTeam&view=modular&view=mNav`
+
+let leagueName = document.getElementById('league')
+let membersList = document.getElementById('members')
+
+
+fetch(draftEndpoint)
+.then(response => response.json())
+.then(myJSON => {
+  console.log(myJSON)
+  appendLeagueInfo(myJSON)
+})
+
+function appendLeagueInfo(myJSON) {
+  leagueName.textContent = myJSON.settings.name
+  
+  for (let i = 0; i < myJSON.members.length; i++) {
+    // console.log(myJSON.members[i])
+    let member = document.createElement('ul')
+    let displayName = document.createElement('li')
+    let name = document.createElement('li')
+    displayName.textContent = myJSON.members[i].displayName
+    name.textContent = myJSON.members[i].firstName + ' ' + myJSON.members[i].lastName
+    membersList.appendChild(member)
+    member.appendChild(displayName)
+    member.appendChild(name)
+  }
+}
